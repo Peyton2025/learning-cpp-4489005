83d6189a7aeeafb67723d42803dd21ddfd3e1cf8
Peyton2025 <123127335+Peyton2025@users.noreply.github.com>
2024-05-31 17:17:38 +0000

    hello world

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hi There!" << std::endl; // endl means endline
+
+    std::cout << std::endl << std::endl;
+    return(0); // 0 means no error came up in program
+}
